1ba683352be5d2534640711124f1f75b20d8045d
Tatyana <idamtanya@gmail.com>
2023-04-02 15:33:55 +0700

    HW3&4
    Я сразу добавила дз 3 и 4. Буду рада вашим комментариям))))))

---
+++ HW3/Doctor.cs
@@ -0,0 +1,49 @@
+﻿namespace HW3
+{
+    public class Doctor : IPerson //Вот тут я использую интерфейс!!!!!
+    {
+
+        public int Experience { get; set; }
+        public string Job { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Doctor(string name, int age, int experience, string job)
+        {
+            Name = name;
+            Age = age;
+            Experience = experience;
+            Job = job;
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, возраст: {Age}, стаж: {Experience}, специализация: {Job}.");
+        }
+
+
+
+        public void Cure(Patient patient)
+        {
+            Console.WriteLine($"Привет, {patient.Name}. Я {Name}, я работаю здесь как {Job}.");
+
+            if (patient.Diagnosis == "Аппендицит" && Job == "Хирург"
+                || patient.Diagnosis == "Хламидиоз" && Job == "Гинеколог"
+                || patient.Diagnosis == "Запор" && Job == "Мед сестра")
+            {
+                if (Experience > 5)
+                {
+                    Console.WriteLine($"У вас действительно {patient.Diagnosis}. Я могу вам помочь.");
+                    patient.AmISick = false;
+                    patient.Diagnosis = "";
+                }
+                else
+                {
+                    Console.WriteLine("У меня не хватает опыта, что бы вылечить тебя :(\n");
+                }
+            } 
+            else { Console.WriteLine("И что ты ко мне пришел? Иди к другому врачу!\n"); }
+        }
+    }
+}
---
+++ HW3/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Создайте классы врачей и пациентов. 
+У каждого врача и пациента должны быть набор свойств и методов (минимум 3 свойства и 2 метода) 
+У врача должен быть метод взаимодействия с пациентом. 
+В классе Program продемонстрируйте взаимодействие моделей врачей и пациентов. Проявите креатив) 
+При проектировании учитывайте принципы SOLID и DRY.  
+
+Например: " У вас есть в вашей больнице 3 врача: терапевт, окулист и невролог, каждый из них имеет стаж, специализацию, имя, возраст и 
+метод Cure, который принимает пациента и, например, меняет у пациента случайным образом статус на "Здоров" или "Болен". У пациента, соответственно,
+в свойствах модели есть имя, возраст, адрес проживания и статус - здоров или болен". */
+
+using HW3;
+
+class Program
+{
+    public static void Main()
+    {
+        Doctor doc1 = new Doctor("Джон", 32, 7, "Гинеколог");
+        Doctor doc2 = new Doctor("Эмма Уотсон", 45, 6, "Мед сестра");
+        Doctor doc3 = new Doctor("Шакира", 27, 2, "Хирург");
+        Doctor doc4 = new Doctor("Карлайл Каллен", 383, 300, "Хирург");
+
+        Patient patient1 = new Patient("Райан Гослинг", 60, true, "Аппендицит");
+        Patient patient2 = new Patient("Охлобыстин", 22, true, "Запор");
+        Patient patient3 = new Patient("Джулия Робертс", 52, true, "Хламидиоз");
+        Patient patient4 = new Patient("Студент СибГМУ", 19, false, "Воспаление хитрости");
+
+        /*Вот тут я использую интерфейс!!!!!
+        doc1.DisplayInfo(); 
+        doc1.Cure(patient4);
+        doc1.Cure(patient3);
+        */
+        List<Doctor> doctors = new List<Doctor>();
+        doctors.Add(doc1);  
+        doctors.Add(doc2);
+        doctors.Add(doc3);
+        doctors.Add(doc4);
+
+        List<Patient> patients = new List<Patient>();
+        patients.Add(patient4);
+        patients.Add(patient1);
+        patients.Add(patient2);
+        patients.Add(patient3);
+
+
+        foreach (Patient patient in patients)
+        {
+            if (patient.AmISick == false) 
+            {
+                Console.WriteLine($"Ах ты бездельник {patient.Name}, топай учиться!!!\n");
+                continue;
+            }
+            patient.Complaint();
+
+            foreach (Doctor doctor in doctors)
+            {
+                doctor.Cure(patient);
+                if (patient.AmISick == false)
+                {
+                    patient.SayThankYou();
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
